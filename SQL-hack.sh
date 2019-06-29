4e234166bc85a6bed44269e9e48070622ddf1a82
FiqraID <51190920+FiqraID@users.noreply.github.com>
2019-06-29 19:20:32 +0700

    Add files via upload

---
+++ SQL-hack.sh
@@ -0,0 +1,50 @@
+blue='\033[34;1m' #biru
+green='\033[32;1m' #ijo
+purple='\033[35;1m' #purple
+cyan='\033[36;1m' #cyan
+red='\033[31;1m' #merah
+white='\033[37;1m' #putih
+yellow='\033[33;1m' #kuning
+orange='\033[1;38;5;208m' #Orange
+echo
+cowsay -f kosh "SQL-hack" | lolcat
+echo $blue"#################################"
+echo $blue" author : fiqra ID"
+echo $blue" team   : B-MCT"
+echo $yellow" fb     : Fiqra ID"
+echo $yellow" ig     : @Fiqra_thanvans"
+echo $yellow"#################################"
+
+echo "MENU SATELIT»"
+echo "1.TELKOM -1" | lolcat
+echo "_________________________"
+echo "2.PALAPA -D 31" | lolcat
+echo "_________________________"
+echo "3.CHINASAT 490" | lolcat
+echo "_________________________"
+echo "4.ASIASAT  -E 04" | lolcat
+echo "_________________________"
+read -p "pilih satelitnya »" nasa
+
+
+if [ $nasa = 1 ] || [ $nasa = 1 ]
+then
+echo "scaning..."
+sleep 3
+echo "nama : TELKOM -1"
+sleep 1
+echo "frekuensi : 35948/48844 Hz"
+sleep 1
+echo "lokasi : 89' bt"
+sleep 1
+echo
+echo "scan....."
+sleep 3
+echo "satelit.go.id/server.php.."
+sleep 4
+echo "tunggu sebentar.."
+sleep 5
+echo "berhasil menggeser"
+sleep 1
+echo "bergeser 29' bt"
+fi
